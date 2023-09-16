1824cb7df59700b0d0f296b33b79d6cc860a448d
Valentin_Silivankou <litin20011995@gmail.com>
2023-09-16 09:38:46 +0300

    Task 2.2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,19 +3,14 @@ const link = document.querySelector('.menu_main');
 link.addEventListener('click', e => {
   const linkTarget = e.target
   if (!linkTarget.classList.contains('menu__link')) return
-  
   const menuItem = linkTarget.closest('.menu__item')
-
   if (!menuItem.querySelector('.menu_sub')) return
   e.preventDefault();
-
   let menuActive = menuItem.querySelector('.menu_active')
-  
   if (menuActive) {
     menuActive.classList.remove('menu_active');
     return
   }
-
   menuActive = link.querySelector('.menu_active')
   if (menuActive) menuActive.classList.remove('menu_active');
   menuItem.querySelector('.menu_sub').classList.add('menu_active')
