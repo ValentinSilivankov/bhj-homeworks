a27c316f6d8c299ea9a298304c111f3757dde914
Valentin_Silivankou <litin20011995@gmail.com>
2023-09-24 20:08:09 +0300

    Task 3.1.

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,46 +1,21 @@
-const ARRAY_LINK = Array.from(document.querySelectorAll(".dropdown__link"));
-const btn = document.querySelector(".dropdown__value");
-
-btn.addEventListener('click', Onclick)
-
-function Onclick() {
-    let classListActive = document.querySelector(".dropdown__list");
-
-    if (classListActive.className == "dropdown__list dropdown__list_active") {
-        classListActive.classList.remove('dropdown__list_active');
-    } else {
-        classListActive.classList.add('dropdown__list_active');
-    }
-}
-
-ARRAY_LINK[0].onclick = function(event){
-    document.querySelector(".dropdown__value").textContent = ARRAY_LINK[0].textContent;
-
-    if (event.target.nodeName != 'A') return;
-    document.querySelector(".dropdown__list").classList.remove('dropdown__list_active');
-    return false;
-}
-
-ARRAY_LINK[1].onclick = function(event){
-    document.querySelector(".dropdown__value").textContent = ARRAY_LINK[1].textContent;
-
-    if (event.target.nodeName != 'A') return;
-    document.querySelector(".dropdown__list").classList.remove('dropdown__list_active');
-    return false;
-}
-
-ARRAY_LINK[2].onclick = function(event){
-    document.querySelector(".dropdown__value").textContent = ARRAY_LINK[2].textContent;
-
-    if (event.target.nodeName != 'A') return;
-    document.querySelector(".dropdown__list").classList.remove('dropdown__list_active');
-    return false;
-}
-
-ARRAY_LINK[3].onclick = function(event){
-    document.querySelector(".dropdown__value").textContent = ARRAY_LINK[3].textContent;
-    
-    if (event.target.nodeName != 'A') return;
-    document.querySelector(".dropdown__list").classList.remove('dropdown__list_active');
-    return false;
-}
+function openMenu() {
+    document.querySelector(".dropdown__list").classList.add("dropdown__list_active");
+  }
+  
+  function closeMenu() {
+    document.querySelector(".dropdown__list").classList.remove("dropdown__list_active");
+  }
+  
+  const VALUE_BTN = document.querySelector(".dropdown__value");
+  
+  VALUE_BTN.addEventListener("click", openMenu);
+  
+  let arrayLink = Array.from(document.querySelectorAll(".dropdown__item"));
+  
+  arrayLink.forEach((item, index) => {
+    item.onclick = () => {
+      closeMenu();
+      VALUE_BTN.textContent = arrayLink[index].textContent;
+      return false;
+    };
+  });
