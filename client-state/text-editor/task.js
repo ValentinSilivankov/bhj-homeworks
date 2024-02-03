8fbcdc75a8df3fb910289c2ea7a40d3966eb64c9
Valentin_Silivankou <litin20011995@gmail.com>
2024-02-03 14:47:17 +0300

    Task 8.1

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,23 @@
+const textarea = document.getElementById('editor')
+
+
+window.addEventListener('load', loadStorage)
+window.addEventListener('beforeunload', saveStorage)
+
+function saveStorage () {
+    if (textarea.value) {
+        localStorage.setItem('text', textarea.value)
+    }
+}
+
+function loadStorage() {
+    textarea.value = localStorage.getItem('text')
+}
+
+const clear = document.querySelector('.clear')
+
+clear.addEventListener('click', (event) => {
+    event.preventDefault()
+    localStorage.removeItem('text')
+    textarea.value = ''
+})
