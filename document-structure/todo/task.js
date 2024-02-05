cfa382c5accc4203aed793706780b9aea6b8d604
Valentin_Silivankou <litin20011995@gmail.com>
2024-02-05 22:01:28 +0300

    Task 6.2.

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,24 +1,56 @@
-const TEXT = document.getElementById("task__input");
-const TASK = document.getElementById("tasks__list");
+function saveTasks() {
+  const tasks = Array.from(document.querySelectorAll('.task__title')).map(task => task.textContent)
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 
-
-TEXT.addEventListener("keydown", (e) => {
-  if (e.key == "Enter" && TEXT.value.length != 0) {
-    TASK.innerHTML += `<div class="task">
-            <div class="task__title">
-              ${TEXT.value}
-            </div>
-            <a href="#" class="task__remove">&times;</a>
-          </div>`;
-    TEXT.value = null;
-    e.preventDefault();
-  }
-});
-
-document.addEventListener("click", delTask);
-function delTask(event) {
-  let tar = event.target;
-  if (Array.from(tar.classList).includes("task__remove")) {
-    tar.parentElement.remove();
+function loadTasks() {
+  const storage = localStorage.getItem("tasks")
+  console.log('storage', storage)
+  if (storage) {
+      const tasks = JSON.parse(storage)
+      tasks.forEach( task => {
+          addTask(task)
+          removeTask()
+      })
   }
 }
+
+function removeTask() {
+  const removes = document.querySelectorAll('.task__remove')
+
+  removes.forEach( remove => {
+      remove.addEventListener("click", (event) => {
+          event.preventDefault()
+          remove.parentElement.remove()
+          saveTasks()
+      })
+  })
+}
+
+function addTask(text) {
+  const tasksContainer = document.getElementById('tasks__list')
+  const taskHTML = `
+            <div class="task">
+              <div class="task__title">${text}</div>
+              <a href="#" class="task__remove">&times;</a>
+            </div>`
+
+  tasksContainer.insertAdjacentHTML('beforeend', taskHTML)
+}
+
+
+window.addEventListener('load', loadTasks)
+
+const form = document.getElementById('tasks__form')
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const input = document.getElementById("task__input")
+  if (input.value) {
+      addTask(input.value)
+      input.value = ''
+
+      saveTasks()
+      removeTask()
+  }
+})
